5d1da2d2df5059d2dc7be0e49af64cee7342950a
clement <cmrtzlff@gmail.com>
2020-11-26 17:29:08 +0100

    feat: script dist message-db installation

---
+++ scripts/install-message-store-in-heroku.js
@@ -0,0 +1,62 @@
+/*
+Normally we could just use the install.sh script in the Eventide
+Message Store repo.  However, those scripts need to create a database and a
+role.  Heroku doesn't allow us to do those functions, so we have this script.
+It just executes the scripts that run after database and role creation.
+*/
+const childProcess = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+function notInEnv(key) {
+  return typeof process.env[key] === 'undefined'
+}
+
+const requiredEnvVars = [
+  'PGHOST',
+  'PGPORT',
+  'PGUSER',
+  'PGPASSWORD',
+  'PGDATABASE',
+]
+const missingEnvVars = requiredEnvVars.filter(notInEnv)
+
+if (missingEnvVars.length > 0) {
+  console.log('The following required variables are missing from your env:\n')
+  console.log(missingEnvVars.join('\n'))
+
+  process.exit(1)
+}
+
+const eventideRootPath = path.join(
+  __dirname,
+  '..',
+  'node_modules',
+  '@eventide',
+  'message-db',
+  'database',
+)
+
+function sqlFilesInDirectory(directory) {
+  const dir = path.join(eventideRootPath, directory)
+
+  return fs.readdirSync(dir).map((file) => path.join(dir, file))
+}
+
+function install() {
+  const sqlFiles = [
+    ...sqlFilesInDirectory('schema'),
+    ...sqlFilesInDirectory('extensions'),
+    ...sqlFilesInDirectory('tables'),
+    ...sqlFilesInDirectory('types'),
+    ...sqlFilesInDirectory('functions'),
+    ...sqlFilesInDirectory('indexes'),
+    ...sqlFilesInDirectory('views'),
+  ]
+
+  sqlFiles.forEach((file) => {
+    childProcess.execSync(`psql -f ${file}`)
+  })
+}
+
+install()
